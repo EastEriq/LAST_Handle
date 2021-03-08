78c0e6bea4193a7e1cfa4b7cdcc3f3f57560fc53
Enrico Segre <enrico.segre@weizmann.ac.il>
2021-03-08 18:19:10 +0000

    getStatus + ConfigStruct

--- +obs/@remoteClass/remoteClass.m
+++ +obs/@remoteClass/remoteClass.m
@@ -5,23 +5,92 @@ classdef remoteClass < obs.LAST_Handle
     %  has been created remotely)
     
     properties
-        Name % the matlab variable name of the object we refer to, in the remote session
-        Messenger % the Messenger object which communicates with session hosting the remote class
+        Name        % the matlab variable name of the object we refer to, in the remote session
+        Messenger   % the Messenger object which communicates with session hosting the remote class
+        %IP
+        %Port
+        %Where char = 'caller';  % define where to execute the command: 'base' | 'caller'
     end
     
     methods
         
-        function R=remoteClass(remotename,messenger)
+        function R=remoteClass(Remotename,varargin)
             % constructor assigning Name and Messenger from optional arguments
-            %  if provided
+            % Usage:
+            % R=remoteClass('CameraMsg',obs.util.Messenger('localhost',8081,8082));
+            % R=remoteClass('CameraMsg','localhost',8081,8082)
+            % The parameters are the Name, destinationIP, destinationPort, localPort
+            
             if nargin>1
-                R.Messenger=messenger;
+                if ischar(varargin{1})
+                    % assume input is e.g., 'localhost',8081,8082
+                    R.Messenger = obs.util.Messenger(varargin{:});
+                    
+                else
+                    % assume user provided a messanger class object
+                    R.Messenger = varargin{1};
+                end
             end
+              
             if nargin>0
-                R.Name=remotename;
+                R.Name = Remotename;
             end
         end
         
+    end
+    
+    methods (Static)
+        function Port=construct_port_number(Type,Mount,Camera)
+            % Construct a port number for device based on its Type, MountNumber and CameraNumber
+            %
+            % Port structure XYYZ
+            % where X -- is type : Mounts 2, Camera 3, Focusers 4, Sensors
+            % 5, Manager 6
+            %       YY -- mount number 1..12
+            %       Z  -- camera/focuser number 1..4, 1..2 for computer
+            
+            % Type Computer DeviceN
+            % 2    01       00       - mount on computer 1
+            % 3    01       04       - camera 4 on computer 1
+            % 3    01       05       - camera 1 listener on computer 1
+            % 4    01       04
+            % 5    01       01
+            % 6    00       00
+            % 6    01       00
+            
+            if nargin<4
+                Computer = [];
+            end
+            
+            switch lower(Type)
+                case 'mount'
+                    TypeInd = 2;
+                case 'camera'
+                    TypeInd = 3;
+                case 'focuser'
+                    TypeInd = 4;
+                case 'sensor'
+                    TypeInd = 5;
+                case 'computer'
+                    TypeInd = 6;
+                    % In this case Camera is ComputerNumber
+                    if mod(Camera,2)==1
+                        % odd computer number
+                        Camera = 1;
+                    else
+                        Camera = 2;
+                    end
+                case 'manager'
+                    TypeInd = 7;
+                otherwise
+                    error('Unknown Type option');
+            end
+            
+            % port number for camera
+            Port = 20000 + TypeInd.*1000 + Mount.*10 + Camera;
+           
+        end
+            
     end
     
 end
--- +obs/classCommand.m
+++ +obs/classCommand.m
@@ -1,7 +1,13 @@
-function result=classCommand(obj,command)
+function Result=classCommand(Obj,Command)
 % Attempt to have an unified way of accessing properties and methods of
 %  classes which may be either local (=within this matlab session) or
 %  remote (i.e defined in a session connected via a couple of Messengers)
+%
+% Input  : - Either a remoreObject, or some device object.
+%            This object must have a Name property.
+%          - A string containing a command to execute.
+%          - Where to execute the command: 'base' | 'caller'.
+%            Default is 'caller'.
 %
 % Examples:
 %
@@ -32,15 +38,27 @@ function result=classCommand(obj,command)
 % but usefulness of such contraptions is questionable 
 
 %  result=classCommand(localMount,'goTo(12,34)')
-if isa(obj,'obs.remoteClass')
-    result=obj.Messenger.query([obj.Name '.' command]);
+
+
+% if nargin<3
+%     Where = 'caller';
+% end
+% switch lower(Where)
+%     case 'caller'
+%         EvalInListener = false;
+%         
+
+if isa(Obj,'obs.remoteClass')
+    Result=Obj.Messenger.query([Obj.Name '.' Command]);
 else
     % how to understand if there is going to be a reply without calling the
     %  command twice?
-    C=obj; % this way we know that we have a temporary object called 'C', for sure
+    C=Obj; % this way we know that we have a temporary object called 'C', for sure
     if nargout>0
-        result=eval(['C.' command]);
+        Result=eval(['C.' command]);
+        %Result=eval([Obj.Name '.' Command]);
+        %Result = eval([C.Name '.' Command]);
     else
-        eval(['C.' command]);
+        Result = eval(['C.' Command]);
     end
 end
