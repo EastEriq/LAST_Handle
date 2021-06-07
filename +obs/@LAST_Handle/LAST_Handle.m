2ffb524236fe26d11e3e5ca4bd55376036c0359e
Enrico Segre <enrico.segre@weizmann.ac.il>
2021-06-07 19:43:02 +0300

    report methods accessible to al subclasses

--- +obs/@LAST_Handle/LAST_Handle.m
+++ +obs/@LAST_Handle/LAST_Handle.m
@@ -6,11 +6,29 @@ classdef LAST_Handle < handle
         Verbose=1;     % textual verbosity. 0=suppress, 1=report info, 2=blabber
     end
     
-    properties (Hidden, GetAccess = public, SetAccess = private)
+    properties (Hidden, GetAccess = public, SetAccess = protected)
         LastError='';  % The last error message
     end
     
-    methods
+    methods(Access = ?obs.LAST_Handle)
+        % the definition is here instead than in separate files in a private/
+        %  folder: so all subclasses which inherit from LAST_Handle can use
+        %  them, otherwise not
+        
+        function report(N,msg)
+            % verbose reporting (to be replaced by a proper call to
+            %  Astropack logger)
+            if N.Verbose
+                fprintf(msg)
+            end
+        end
+
+        function reportError(I,msg)
+            % report on stdout and set LastError, with the same argument
+            I.LastError=msg;
+            I.report([msg,'\n'])
+        end
+
     end
     
 end
--- +obs/@LAST_Handle/private/report.m
+++
@@ -1,6 +0,0 @@
-function report(N,msg)
-% verbose reporting
-    if N.Verbose
-        fprintf(msg)
-    end
-end
