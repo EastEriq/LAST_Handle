261be98ac024f50a25ffe36412ffaf26b2a838a3
Enrico Segre <enrico.segre@weizmann.ac.il>
2021-06-07 13:41:38 +0300

    default Verbose is 1

--- +obs/@LAST_Handle/LAST_Handle.m
+++ +obs/@LAST_Handle/LAST_Handle.m
@@ -3,7 +3,7 @@ classdef LAST_Handle < handle
     properties (Hidden)
         Config         % name of the associated configuration file (searched in the configurations directory)
         ConfigStruct   % The configuration structure loaded from file
-        Verbose        % textual verbosity. 0=suppress, 1=report info, 2=blabber
+        Verbose=1;     % textual verbosity. 0=suppress, 1=report info, 2=blabber
     end
     
     properties (Hidden, GetAccess = public, SetAccess = private)
