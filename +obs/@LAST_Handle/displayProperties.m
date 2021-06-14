db07208dbf4b9e898fcb8cc848875096f28cfde5
Enrico Segre <enrico.segre@weizmann.ac.il>
2021-06-14 19:31:32 +0300

    comment about nonreadable properties

--- +obs/@LAST_Handle/displayProperties.m
+++ +obs/@LAST_Handle/displayProperties.m
@@ -2,11 +2,15 @@ function displayProperties(X)
 % Displays on shell the values of all properties, visible and hidden,
 %  without distinction. The trick comes from
 %  https://undocumentedmatlab.com/articles/accessing-private-object-properties
+%
+% Note: properties whose values cannot be displayed (e.g. because their getter
+%  silently errors out) are not shown at all. Use .listAllProperties
+%  instead, to discover about their existence
 
-    % temporarily squelch the warning on casting  class to structure
+    % temporarily squelch the warning on casting class to structure
     id='MATLAB:structOnObject';
     presentstate=warning('query',id);
     warning('off',id)
     disp(struct(X))
-    warning(presentstate.state,id); %restore state
+    warning(presentstate.state,id); %restore warning state
 end
