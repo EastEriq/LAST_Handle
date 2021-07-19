6a3a9c6c2252054d7a790f83e32626824e46bfb1
Enrico Segre <enrico.segre@weizmann.ac.il>
2021-07-19 16:42:37 +0300

    displayProperties with optional return argument

--- +obs/@LAST_Handle/displayProperties.m
+++ +obs/@LAST_Handle/displayProperties.m
@@ -1,16 +1,19 @@
-function displayProperties(X)
+function result=displayProperties(X)
 % Displays on shell the values of all properties, visible and hidden,
 %  without distinction. The trick comes from
 %  https://undocumentedmatlab.com/articles/accessing-private-object-properties
 %
 % Note: properties whose values cannot be displayed (e.g. because their getter
 %  silently errors out) are not shown at all. Use .listAllProperties
-%  instead, to discover about their existence
+%  instead, to discover their existence
 
     % temporarily squelch the warning on casting class to structure
     id='MATLAB:structOnObject';
     presentstate=warning('query',id);
     warning('off',id)
-    disp(struct(X))
+    result=struct(X);
     warning(presentstate.state,id); %restore warning state
+    if nargout==0
+        disp(result)
+    end
 end
