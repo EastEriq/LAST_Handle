a8dac8d5423f4b36ed29a96d4ffb6ba81bb27bce
Enrico Segre <enrico.segre@weizmann.ac.il>
2021-08-09 19:47:34 +0300

    note TODO classCommand(WaitReply=false)

--- +obs/@LAST_Handle/classCommand.m
+++ +obs/@LAST_Handle/classCommand.m
@@ -36,6 +36,9 @@ function Result=classCommand(Obj,Command,Where)
 %  
 % but usefulness of such contraptions is questionable 
 
+% TODO: instead of Where, for which I don't see any more an use case,
+%       add an argument WaitReply. If false, and nargout=0, use
+%       Messenger.send instead of .query [or suppress Result altogether]
 if nargin<3
     Where = 'caller';
 end
