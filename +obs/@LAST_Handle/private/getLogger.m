309b51987a79e3932f474949d82c7e52b95b8f56
Enrico Segre <>
2024-03-31 14:26:15 +0000

    adapted to the current ApiLogger

--- +obs/@LAST_Handle/private/getLogger.m
+++ +obs/@LAST_Handle/private/getLogger.m
@@ -1,11 +1,14 @@
 function ChosenLogger=getLogger(L)
 % a helper for the .reportXXX methods, to decide which logger is in use
     global DefaultLogger
+    
     if isempty(DefaultLogger)
-        DefaultLogger = obs.api.ApiLogger('FilePath', 'no-location');
+        DefaultLogger = MsgLogger();
     end
     if isprop(L, 'Logger') && ~isempty(L.Logger)
         ChosenLogger = L.Logger;
     else
         ChosenLogger = DefaultLogger;
     end
+    % fprintf("Class(L): '%s', ChosenLogger: '%s'\n", class(L), class(ChosenLogger));
+end
