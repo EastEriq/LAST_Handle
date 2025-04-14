7829b097a9ab3abff3f5b4018fc13017f86d59fc
The LAST Ocs <>
2025-04-14 13:07:08 +0000

    index array values in pushPVvalue (#2)

--- +obs/@LAST_Handle/LAST_Handle.m
+++ +obs/@LAST_Handle/LAST_Handle.m
@@ -200,18 +200,30 @@ classdef LAST_Handle < handle
             if ~isempty(L.PVstore)
                 stack=dbstack();
                 fun=stack(2).name;
-                key=sprintf('%s:%s',fun,L.Id);
                 t=(now-datenum(1970,1,1))*86400; % timezone ignored but locale should be UTC
-                try
-                    if raw
-                        L.PVstore.hset(key,'t',t,'v',value);
+                % individual push of components for vector values
+                if ~isa(value,'char') && numel(value)>1
+                    N=numel(value);
+                else
+                    N=1;
+                end
+                for i=1:N
+                    if N>1
+                        key=sprintf('%s:%s_%d',fun,L.Id,i);
                     else
-                        L.PVstore.hset(key,'t',t,'v',jsonencode(value));
+                        key=sprintf('%s:%s',fun,L.Id);
                     end
-                    % set one day for expiration (could also not)
-                    L.PVstore.expire(key,86400);
-                catch PushError
-                    L.reportError('cannot push to PVstore %s : %s',key,PushError.message)
+                    try
+                        if raw
+                            L.PVstore.hset(key,'t',t,'v',value(i));
+                        else
+                            L.PVstore.hset(key,'t',t,'v',jsonencode(value(i)));
+                        end
+                        % set one day for expiration (could also not)
+                        L.PVstore.expire(key,86400);
+                    catch PushError
+                        L.reportError('cannot push to PVstore %s : %s',key,PushError.message)
+                    end
                 end
             end
         end
